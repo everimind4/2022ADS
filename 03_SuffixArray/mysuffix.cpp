6501ee1d6988eeb219da6b2de8ddc6eda3e306d5
everimind4 <everimind4@gmail.com>
2022-04-27 20:15:51 +0900

    Suffix Array
    compare 함수 오타 수정, search 파라미터 변경

--- 03_SuffixArray/mysuffix.cpp
+++ 03_SuffixArray/mysuffix.cpp
@@ -12,7 +12,7 @@ void buildsa();                     // 접미사 배열을 생성하는 함수
 bool compare(int, int);             // 그룹 비교를 수행하는 함수
 void buildlcp();                    // LCP 배열을 생성하는 함수
 int buildlr(int, int);              // LCP_LR 배열을 생성하는 함수
-int search(int, int);               // 문자열 T에서 패턴을 찾는 함수
+int search(string);                 // 문자열 T에서 패턴을 찾는 함수
 
 // 접미사 배열, LCP 배열 저장 및 그룹을 위한 변수
 int sa[N], sa_i[N], lcp[N], lcp_lr[N], group[N], nextgroup[N], l;
@@ -83,7 +83,7 @@ void buildsa() {                            // 접미사 배열 생성
 
 bool compare(int i, int j) {                // Prefix Doubling을 위한 비교 결과 반환 함수
     if (group[i] != group[j])               // 특정 부분문자열의 순위가 동일한 경우
-        return group[i+l] < group[j];       // 순위 비교 결과를 바로 반환
+        return group[i] < group[j];         // 순위 비교 결과를 바로 반환
     else                                    // 해당 부분문자열의 순위가 다를 경우
         return group[i+l] < group[j+l];     // 그 다음 부분문자열의 순위를 비교하여 결과 반환
 }
