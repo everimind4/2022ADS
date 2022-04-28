f755186a7acdeac5333f871a710db9a6b195d423
everimind4 <everimind4@gmail.com>
2022-04-28 20:23:33 +0900

    Normal Find
    시간 측정 방식 개선

--- 03_SuffixArray/normalfind.cpp
+++ 03_SuffixArray/normalfind.cpp
@@ -11,6 +11,9 @@ int search(string); // 문자열 T에서 패턴을 찾는 함수
 int n, k, s, m;     // 문자열 T의 길이, 질의 횟수, 패턴의 시작 위치, 길이
 string t, p;    // 문자열 T와 패턴 문자열을 저장
 
+chrono::system_clock::time_point start, finish; // 시간 측정을 위한 변수
+chrono::microseconds duration;  // 시간 차이를 계산하기 위한 변수
+
 int main() {
     ifstream fin("indata.txt");     // 데이터 입력 파일 open
     if (!fin.is_open()) {           // File이 존재하지 않을 경우
@@ -19,22 +22,21 @@ int main() {
     }
     ofstream fout("outdata.txt");   // 결과 출력 파일 open
 
-    chrono::system_clock::time_point start, finish; // 시간 측정을 위한 변수
-    chrono::microseconds duration;  // 시간 차이를 계산하기 위한 변수
 
     fin >> n >> k >> t;             // 파일에서 해당 변수들의 값을 Read
 
-    start = chrono::system_clock::now();    // 시간 측정 시작
-    for (int i = 0; i < k; i++) {           // k번 반복하며
-        fin >> s >> m;                      // 패턴의 시작 위치와 길이 입력
-        p = t.substr(s, m);                 // 시작 위치와 길이로 패턴 추출
-        fout << search(p) << endl;          // 패턴 등장 횟수 탐색 후 출력
+    int count;
+    for (int i = 0; i < k; i++) {               // k번 반복하며
+        fin >> s >> m;                          // 패턴의 시작 위치와 길이 입력
+        p = t.substr(s, m);                     // 시작 위치와 길이로 패턴 추출
+        start = chrono::system_clock::now();    // 시간 측정 시작
+        count = search(p);                      // 패턴 문자열 탐색 후 횟수 저장
+        finish = chrono::system_clock::now();   // 시간 측정 끝
+        duration += chrono::duration_cast<chrono::microseconds>(finish - start);
+        fout << count << endl;                  // 패턴 등장 횟수를 파일에 출력
     }
-    finish = chrono::system_clock::now();   // 시간 측정 끝
-    duration = chrono::duration_cast<chrono::microseconds>(finish - start);
     cout << "패턴 문자열 탐색 소요 시간 : " << duration.count() << " μs" << endl;
     return 0;
-
 }
 
 int search(string p) {                      // 문자열 T에서 패턴 P를 탐색해 등장 횟수를 반환하는 함수
