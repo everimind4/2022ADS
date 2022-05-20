9e6b76679c85791b9db218edcf003953802b1dc1
everimind4 <everimind4@gmail.com>
2022-05-20 15:20:56 +0900

    Priority Queue
    파일에서 데이터를 입력받는 코드 추가

--- 04_PriorityQueue/intersect.cpp
+++ 04_PriorityQueue/intersect.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <queue>
 
 using namespace std;
@@ -9,16 +10,40 @@ struct segment {
     long xe;
     long ys;
     long ye;
-}
+
+    segment(int N, long a, long b, long c, long d)
+         : num(N), xs(a), xe(b), ys(c), ye(d) {}
+};
 
 struct compare {
     bool operator()(const segment& seg1, const segment& seg2) {
         return seg1.xs > seg2.xs;
     }
-}
+};
 
 priority_queue<segment, vector<segment>, compare> pq;
 
+int n, xs, xe, ys, ye;
+
 int main() {
+    ifstream fin("in_segment.txt");
+    if (!fin.is_open()) {
+        cout << "There is no 'indata.txt' file . . ." << endl;
+        return -1;
+    }
+    fin >> n;
+    for (int i = 0; i < n; i++) {
+        fin >> xs >> xe >> ys >> ye;
+        pq.push(segment(i+1, xs, xe, ys, ye));
+    }
+    while (!pq.empty()) {
+        segment temp = pq.top();
+        cout << temp.num << ' '
+            << temp.xs << ' '
+            << temp.xe << ' '
+            << temp.ys << ' '
+            << temp.ye << endl;
+        pq.pop();
+    }
     return 0;
 }
