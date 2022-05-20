b4214c62e6ca20909329dd1bf2ed71cdd93e02fa
everimind4 <everimind4@gmail.com>
2022-05-20 17:02:01 +0900

    Priority Queue
    Line Segment 교차 판단을 위한 함수 구현

--- 04_PriorityQueue/intersect.cpp
+++ 04_PriorityQueue/intersect.cpp
@@ -7,12 +7,12 @@ using namespace std;
 struct segment {
     int num;
     long xs;
-    long xe;
     long ys;
+    long xe;
     long ye;
 
     segment(int N, long a, long b, long c, long d)
-         : num(N), xs(a), xe(b), ys(c), ye(d) {}
+         : num(N), xs(a), ys(b), xe(c), ye(d) {}
 };
 
 struct compare {
@@ -21,9 +21,10 @@ struct compare {
     }
 };
 
-priority_queue<segment, vector<segment>, compare> pq;
+bool intersect(const segment&, const segment&);
 
-int n, xs, xe, ys, ye;
+priority_queue<segment, vector<segment>, compare> pq;
+int n, xs, ys, xe, ye;
 
 int main() {
     ifstream fin("in_segment.txt");
@@ -31,11 +32,13 @@ int main() {
         cout << "There is no 'indata.txt' file . . ." << endl;
         return -1;
     }
+
     fin >> n;
     for (int i = 0; i < n; i++) {
-        fin >> xs >> xe >> ys >> ye;
+        fin >> xs >> ys >> xe >> ye;
         pq.push(segment(i+1, xs, xe, ys, ye));
     }
+
     while (!pq.empty()) {
         segment temp = pq.top();
         cout << temp.num << ' '
@@ -45,5 +48,18 @@ int main() {
             << temp.ye << endl;
         pq.pop();
     }
+
     return 0;
+}
+
+bool intersect(const segment &a, const segment &b) {
+    long sa1, sa2, sa3, sa4;
+    sa1 = (a.xs*a.ye + a.xe*b.ys + b.xs*a.ys) - (a.ys*a.xe + a.ye*b.xs + b.ys*a.xs);
+    sa2 = (a.xs*a.ye + a.xe*b.ye + b.xe*a.ys) - (a.ys*a.xe + a.ye*b.xe + b.ye*a.xs);
+    sa3 = (b.xs*b.ye + b.xe*a.ys + a.xs*b.ys) - (b.ys*b.xe + b.ye*a.xs + a.ys*b.xs);
+    sa4 = (b.xs*b.ye + b.xe*a.ye + a.xe*b.ys) - (b.ys*b.xe + b.ye*a.xe + a.ye*b.xs);
+    if (sa1*sa2 < 0 && sa3*sa4 < 0)
+        return true;
+    else
+        return false;
 }
