db55bcde7b055f0e3ff519cc739c106271011e0e
everimind4 <everimind4@gmail.com>
2022-06-09 16:16:09 +0900

    Binomial Heap
    입출력 코드, 함수 원형 작성

---
+++ 05_BinomialQueue/intersect.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+#include <queue>
+
+using namespace std;
+
+void merge(node*, node*);
+void insert(int);
+void pop();
+
+struct node {
+    int key;
+    int deg;
+    string name;
+    node *down, *next;
+
+    node(int key, string name) : key(key), deg(0), name(name), down(nullptr), next(nullptr) {};
+}
+
+int main() {
+    ifstream fin("bhin.txt");       // 입력 파일 Open
+    if (!fin.is_open()) {           // 열리지 않았을 경우
+        cout << "There is no 'bhin.txt' file . . ." << endl;
+        return -1;                  // 예외처리
+    }
+    ofstream fout("bhout.txt");     // 출력 파일 Open
+
+    string line;                    // 한 줄씩 저장할 string 변수
+    while (fin.peek() != EOF) {     // 파일 전체를 읽어들이는 동안
+        getline(fin, line);         // 한 줄씩 읽어서 line에 저장
+        cout << line << endl;
+    }
+}
