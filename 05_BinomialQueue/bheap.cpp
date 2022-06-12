abaa96f61fe3271b279d34227b4c82a3eb6863b7
everimind4 <everimind4@gmail.com>
2022-06-12 12:58:18 +0900

    Binomial Queue
    Operation Routine 개선 및 디버깅, 파일 출력 코드 작성

--- 05_BinomialQueue/bheap.cpp
+++ 05_BinomialQueue/bheap.cpp
@@ -23,13 +23,14 @@ void merge(node*, node*);
 
 node *bheap = nullptr;
 
+ifstream fin("bhin.txt");       // 입력 파일 Open
+ofstream fout("bhout.txt");     // 출력 파일 Open
+
 int main() {
-    ifstream fin("bhin.txt");       // 입력 파일 Open
     if (!fin.is_open()) {           // 열리지 않았을 경우
         cout << "There is no 'bhin.txt' file . . ." << endl;
         return -1;                  // 예외처리
     }
-    ofstream fout("bhout.txt");     // 출력 파일 Open
 
     string line;                    // 한 줄씩 저장할 string 변수
     while (fin.peek() != EOF) {     // 파일 전체를 읽어들이는 동안
@@ -50,38 +51,61 @@ int main() {
 
 void insert(int key, string name) {
     node *temp = new node(key, name);
-    if (bheap == nullptr)
+    if (bheap == nullptr || bheap->deg != 0) {
+        temp->next = bheap;
         bheap = temp;
-    else
+    }
+    else {
         merge(bheap, temp);
+    }
 }
 
 void pop() {
-    node *temp, *down, *prev = nullptr;
-    int min = top(bheap);
+    if (bheap == nullptr)
+        return;
+    int key_min = top(bheap);
+    fout << key_min << ' ';
     priority_queue<string, vector<string>, greater<string>> res;
-    while (top(bheap) == min) {
-        for (temp = bheap; temp != nullptr; temp = temp->next) {
-            if (min == temp->key) {
-                res.push(temp->name);
-                if (prev != nullptr)
-                    prev->next = temp->next;
-                down = temp->down;
-                delete(temp);
-                merge(bheap, down);
+    while (key_min == top(bheap)) {
+        node *temp = bheap, *min = bheap, *prev = nullptr, *prev_min = nullptr;
+        while (temp != nullptr) {
+            if (min->key > temp->key) {
+                min = temp;
+                prev_min = prev;
+            }
+            prev = temp;
+            temp = temp->next;
+        }
+        if (prev_min != nullptr)
+            prev_min->next = min->next;
+        else
+            bheap = min->next;
+        temp = min->down;
+        res.push(min->name);
+        delete min;
+        while (temp != nullptr) {
+            prev = temp;
+            temp = temp->next;
+            if (bheap == nullptr || bheap->deg != 0) {
+                prev->next = bheap;
+                bheap = prev;
             } else {
-                prev = temp;
-                temp = temp->next;
+                merge(bheap, prev);
             }
         }
     }
+    while (!res.empty()) {
+        fout << res.top() << ' ';
+        res.pop();
+    }
+    fout << endl;
 }
 
 int top(node* bheap) {
     if (bheap == nullptr)
         return 0;
-    node *temp;
     int min = 100000;
+    node *temp;
     for (temp = bheap; temp != nullptr; temp = temp->next)
         if (min > temp->key)
             min = temp->key;
@@ -89,23 +113,38 @@ int top(node* bheap) {
 }
 
 void merge(node* a, node* b) {
-    node *ta = a, *tb = b, *root, *down;
-    while (tb != nullptr) {
-        while (ta != nullptr) {
-            if (ta->deg == tb->deg) {
-                if (ta->key > tb->key) {
-                    root = tb;
-                    down = ta;
-                } else {
-                    root = ta;
-                    down = tb;
-                }
-                down->next = root->down;
-                root->down = down;
-                root->deg++;
+    node *prev = nullptr;
+    while (a != nullptr && a->deg < b->deg) {
+        prev = a;
+        a = a->next;
+    }
+    if (a == nullptr) {
+        prev->next = b;
+        b->next = nullptr;
+    } else if (a->deg != b->deg) {
+        if (prev != nullptr)
+            prev->next = b;
+        b->next = a;
+    } else {
+        b->next = a->next;
+        while (b != nullptr && a->deg == b->deg) {
+            if (a->key > b->key) {
+                if (prev != nullptr)
+                    prev->next = b;
+                else
+                    bheap = b;
+                b->deg++;
+                a->next = b->down;
+                b->down = a;
+                a = b;
+                b = b->next;
+            } else {
+                a->deg++;
+                a->next = b->next;
+                b->next = a->down;
+                a->down = b;
+                b = a->next;
             }
-            ta = ta->next;
         }
-        tb = tb->next;
     }
 }
